919cd7db6a27d62dbf3df1625bbf06355367fc4b
c0a2106774 <c0a2106774@edu.teu.ac.jp>
2023-11-21 15:48:47 +0900

    練習5

--- pygame_image.py
+++ pygame_image.py
@@ -9,14 +9,14 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg") #練習1
     kkimg = pg.image.load("ex01/fig/3.png")
     kkimg = pg.transform.flip(kkimg, True, False)
-    kkimgs = [kkimg,pg.transform.rotozoom(kkimg, 10, 1.0)]
+    kkimgs = [kkimg,pg.transform.rotozoom(kkimg, 10, 1.0)] #練習3 こうかとんsurfaceのリスト
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0]) #練習4　背景画像を表示
-        screen.blit(kkimgs[1],[300,200])
+        screen.blit(kkimgs[tmr%2],[300,200])#練習5 こうかとんはばたく
         pg.display.update()
         tmr += 1        
         clock.tick(10)
